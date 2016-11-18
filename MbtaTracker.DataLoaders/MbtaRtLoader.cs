f53faca40d3d8902f174e8d582839b6faa2eda3a
Edmund Schweppe <schweppe@ieee.oreg>
2016-11-18 13:57:04 -0500

    only request predictions for routes with scheduled trips (keeps CapeFlyer from breaking API call)

--- MbtaTracker.DataLoaders/MbtaRtLoader.cs
+++ MbtaTracker.DataLoaders/MbtaRtLoader.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -18,33 +19,65 @@ namespace MbtaTracker.DataLoaders
         {
             Trace.TraceInformation("getting json");
             string json = GetPredictionsByRoutesJson(ApiKey, ConnectionString).Result;
+
+            LoadJson(json);
+        }
+
+        public void LoadFromFile(string jsonFilePath)
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            LoadJson(json);
+        }
+
+        private void LoadJson(string json)
+        {
+            Trace.TraceInformation("loading json");
             Prediction p = new Prediction
             {
                 prediction_time = DateTime.UtcNow,
                 prediction_json = json
             };
-            Trace.TraceInformation("loading json");
-
             p.LoadFromJson();
+
             Trace.TraceInformation("saving to database");
             using (var db = new MbtaTrackerDb(ConnectionString))
             {
                 db.Predictions.Add(p);
                 db.SaveChanges();
             }
-
         }
+
         private async Task<string> GetPredictionsByRoutesJson(string apiKey, string connStr)
         {
+            /*
+             select distinct t.route_id 
+from GtfsStatic.Trips t
+join GtfsStatic.Calendar c on c.service_id = t.service_id
+where t.download_id = 3
+and c.download_id = 3
+and getdate() between c.start_date and c.end_date
+             
+             */
             string[] routes;
+            //using (var db = new MbtaTrackerDb(connStr))
+            //{
+            //    routes = db.Routes.Select(r => r.route_id).ToArray();
+            //}
             using (var db = new MbtaTrackerDb(connStr))
             {
-                routes = db.Routes.Select(r => r.route_id).ToArray();
+                routes = db.Trips
+                    .Join(db.Calendars.Where( c=> c.start_date <= DateTime.Today && DateTime.Today <= c.end_date),
+                        t => new { t.download_id, t.service_id },
+                        c => new { c.download_id, c.service_id },
+                        (t, c) => t.route_id
+                        )
+                    .Distinct()
+                    .ToArray();
             }
-            //string[] routes = { "CR-Fitchburg", "CR-Newburyport", "CR-Lowell", "CR-Haverhill" };
-            string url = String.Format("http://realtime.mbta.com/developer/api/v2/predictionsbyroutes?api_key={0}&routes={1}&format=json",
-                 apiKey,
-                 String.Join(",", routes));
+                //string[] routes = { "CR-Fitchburg", "CR-Newburyport", "CR-Lowell", "CR-Haverhill" };
+                string url = String.Format("http://realtime.mbta.com/developer/api/v2/predictionsbyroutes?api_key={0}&routes={1}&format=json",
+                     apiKey,
+                     String.Join(",", routes));
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(url);
